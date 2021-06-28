3f275fef56853d81120176add10a853a9e50baa8
legossol <wkdgothf@gmail.com>
2021-06-29 04:02:09 +0900

    recursiveFunctionGugudanMaking

--- src/algorithm/recursivefunction.java
+++ src/algorithm/recursivefunction.java
@@ -1,15 +1,29 @@
 package algorithm;
 class sum{
-    public static int getSum(int n){
-        if(n == 1 ){
+    public static int getSum(int n) {
+        if (n == 1) {
             return 1;
         }
-        return n + getSum(n -1);
+        return n + getSum(n - 1);
     }
 }
+class gugu{
+    public static void rcGugudan(int n){
+        if(n==0){
+            System.out.println("Done");
+        }else{
+            System.out.printf("2 x %d = %d",n,2*n);
+            System.out.println();
+            gugu.rcGugudan(n-1);
+        }
+
+    }
+}
+
 public class recursivefunction{
     public static void main(String[] args) {
         int i = sum.getSum(10);
         System.out.println(i);
+        gugu.rcGugudan(9);
     }
 }
