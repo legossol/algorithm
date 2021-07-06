c0092aacdcd09ec3049b5cc79c82c954f0adb0c3
legossol <wkdgothf@gmail.com>
2021-07-06 15:05:24 +0900

    jade templete engine grammer

--- whatIsNode/app.js
+++ whatIsNode/app.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = express();
+app.set('view engine','jade');//제이드 템플릿 엔진을 사용하겠다
+app.set('views','./jadeviews');//이걸 설정안하면 위에 제이드 템플릿 엔진 set이 자동으로 views라는 폴더를 추적한다.
+app.get('/templete',(req,res)=>{//templete를 치고 들어오는 유저에게 이 함수가 실행되며 temp라는 파일ㅇ르 보여줄것이다.
+    res.render('temp',{time:Date()});
+})
+
+app.locals.pretty =true;
 app.use(express.static('image'));
 app.get('/',(req,res)=>{
     res.send("어 왔니?");
@@ -26,3 +33,4 @@ app.get('/dynamic', (req,res)=>{
     </html>`
     res.send(output)
 })
+
