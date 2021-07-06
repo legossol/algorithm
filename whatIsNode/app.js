9039a508b1d217160edfb7b1ff23458105c0e5a6
legossol <wkdgothf@gmail.com>
2021-07-06 14:43:33 +0900

    static dynamic node

--- whatIsNode/app.js
+++ whatIsNode/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use(express.static('image'));
 app.get('/',(req,res)=>{
     res.send("어 왔니?");
 });
@@ -9,3 +10,19 @@ app.get('/login',(req,res)=>{
 app.listen(3000,()=>{
     console.log('Connected 3000 port');
 });
+app.get('/dynamic', (req,res)=>{
+    var time =Date();
+    var output =`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>static dynamic page</p>
+        ${time}
+    </body>
+    </html>`
+    res.send(output)
+})
