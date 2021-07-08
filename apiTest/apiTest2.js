b25d2078b28561682e23ee8b1b83acabf43cc262
legossol <wkdgothf@gmail.com>
2021-07-08 17:13:03 +0900

    apiTest api get id done

--- apiTest/apiTest2.js
+++ apiTest/apiTest2.js
@@ -2,17 +2,22 @@ const express = require('express');
 const app = express();
 const users = require('./api.json');
 
-const server = app.listen(3002, () =>{
+const server = app.listen(3001, () =>{
     console.log(`Start Server : localhost:3001`)
 });
 //1. 쿼리스트링으로 아이디 받np아서 같으면 출력하기
 
-app.get('/api',(req, res, next)=>{
-    console.log(users);
+app.get('/api/:id',(req, res)=>{
     const userId = req.params.id;
-    const result ={};
+    let result ={};
     try{
-
+        for(let i=0;i<users.length;i++){
+            if(userId == users[i].id){
+                result = users[i];
+                console.log(result);
+                res.send(result);
+            }
+        }
     }catch(err){
         res.send("err발생" + err)
     }
