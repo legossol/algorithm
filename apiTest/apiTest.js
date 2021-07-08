472e4f313621bcab3feb8d53e35ed1abb2963926
legossol <wkdgothf@gmail.com>
2021-07-08 16:51:02 +0900

    apiTest reSetting cuz on json import problem

--- apiTest/apiTest.js
+++ apiTest/apiTest.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const app = express.Router();
-const users = app.get('api.Json');
+const app = express();
+const users = require('./api.json');
+
+const server = app.listen(3001, () =>{
+    console.log(`Start Server : localhost:3001`)
+});
 
 //1. 나이 30 이상, 회사이름 카테노이드 추출
 //2. 이름 뒤집기
 app.get('/api',(req, res)=>{
+    console.log(users)
     
+    try{
+        res.send(users)
+    }catch(err){
+        res.send("err발생" + err)
+    }
 })
--- apiTest/apiTest2.js
+++ apiTest/apiTest2.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const app = express.Router();
-const users = app.get('api.Json');
+const app = express();
+const users = require('./api.json');
 
-//1. 쿼리스트링으로 아이디 받아서 같으면 출력하기
+const server = app.listen(3002, () =>{
+    console.log(`Start Server : localhost:3001`)
+});
+//1. 쿼리스트링으로 아이디 받np아서 같으면 출력하기
 
 app.get('/api',(req, res, next)=>{
-    
+    console.log(users);
+    const userId = req.params.id;
+    const result ={};
+    try{
+
+    }catch(err){
+        res.send("err발생" + err)
+    }
 })
