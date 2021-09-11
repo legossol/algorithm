88ba005efb86971bd955b60c6ebffe186e7beda6
legossol <wkdgothf@gmail.com>
2021-09-11 11:23:31 +0900

    work

---
+++ algorithmJAVA/src/hashSet/SetSample.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetSample{
+    public static void main(String[] args) {
+        SetSample sample = new SetSample();
+        String []cars =new String[]{
+                "Tico","Sonata","BMW","Benz","Lexus","Lexus","Tico"
+        };
+        System.out.println(sample.getCarKinds(cars));
+    }
+    public int getCarKinds(String[] cars){
+        Set<String> setCars = new HashSet<String>();
+        for(String car : cars){
+            setCars.add(car);
+        }
+        return setCars.size();
+
+    }
+}
+
+//배열로 표시하는 방법
+public class SetSample{
+    public static void main(String[] args) {
+        SetSample sample = new SetSample();
+        String []cars =new String[]{
+                "Tico","Sonata","BMW","Benz","Lexus","Lexus","Tico"
+        };
+        System.out.println(sample.getCarKinds(cars));
+    }
+    public Set<String> getCarKinds(String[] cars){
+        Set<String> setCars = new HashSet<String>();
+        for(String car : cars){
+            setCars.add(car);
+        }
+        return setCars;
+
+    }
+}
